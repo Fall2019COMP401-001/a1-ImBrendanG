e4817daf7a165037a1a4628c92d3127f2e006141
ImBrendanG <54329436+ImBrendanG@users.noreply.github.com>
2019-08-29 13:26:02 -0400

    Add files via upload

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -7,8 +7,75 @@ public class A1Jedi {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		// Declare variables
+		int differentItems = 0;
+		int customers = 0;
+		String firstName = "";
+		String lastName = "";
+		int customerItems = 0;
+		int itemAmount = 0;
+		String itemName = "";
+		
+		
+		// Reads amount of different items
+		differentItems = scan.nextInt();
+				
+		// Sets array lengths for items and prices and customer counter
+		String[] itemNames = new String[differentItems];
+		double[] itemPrices = new double[differentItems];
+		int[] itemCounter = new int[differentItems];
+		int[] customerCounter = new int[differentItems];
+		
+		
+		// Goes through item list and prices
+		for(int i = 0; i < differentItems; i++) {
+					
+			itemNames[i] = scan.next();
+			itemPrices[i] = scan.nextDouble();
+					
+		}
+		
+		customers = scan.nextInt();
+		
+		// Goes through each customers
+		for(int j = 0; j < customers; j++) {
+					
+			firstName = scan.next();
+			lastName = scan.next();
+			customerItems = scan.nextInt();
+					
+			// Goes through the items of customer
+			for(int k = 0; k < customerItems; k++) {
+				
+				itemAmount = scan.nextInt();
+				itemName = scan.next();
+				
+				for(int l = 0; l < differentItems; l++) {
+					if(itemName.equals(itemNames[l])) {
+						customerCounter[l] += 1;
+						itemCounter[l] += itemAmount;
+					}
+				}
+				
+				
+				
+				
+						
+				
+			}
+		}
+		
+		// Prints how many customers bought an item and how many of that item were bought
+		for(int i = 0; i < differentItems; i++) {
+			
+			if(customerCounter[i] == 0) {
+				System.out.println("No customers bought " + itemNames[i]);
+			}
+			else {
+				System.out.println(customerCounter[i] + " customers bought " + itemCounter[i] + " " + itemNames[i]);
+			}
+			
+		}
 		
 	}
 }
